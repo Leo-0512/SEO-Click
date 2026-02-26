0ae64ceba8eee976c1c74f5bf9b94df7f418c1a3
Leo-0512 <manishjainpvt@gmail.com>
2026-02-26 22:48:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from playwright.async_api import async_playwright
 async def seo_bot():
     async with async_playwright() as p:
         # ब्राउज़र लॉन्च (Headless mode)
-        browser = await p.chromium.launch(headless=True)
+        browser = await p.chromium.launch(headless=True,args=["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"])
         context = await browser.new_context(
             user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
         )
